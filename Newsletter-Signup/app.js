8c0b5f911542ad61e11a69476e225036f29223fc
silvergun8291 <sv5506829sv@gmail.com>
2022-07-12 23:16:04 +0900

    2022-7-12

---
+++ Newsletter-Signup/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const https = require('https');
+const bodyParser = require("body-parser");
+
+const app = express();
+
+
+app.get("/", function(req, res) {
+
+})
+
+
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000.");
+})
